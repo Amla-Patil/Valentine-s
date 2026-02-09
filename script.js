943d3916773069abbcfd792921da26595fa37bd1
Amla-Patil <patilamla@gmail.com>
2026-02-09 23:44:16 +0530

    Add mouseover effect to noBtn element

---
+++ script.js
@@ -0,0 +1,11 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+function goYes() {
+  window.location.href = "date.html";
+}
